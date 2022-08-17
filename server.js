5b307695b7e5f7745d18dea702bcd616907a5715
ARJUN <arjunsharman06@gmail.com>
2022-08-17 14:02:25 -0400

    heroku

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const sess = {
   secret: 'Super secret secret',
@@ -22,70 +30,14 @@ const sess = {
 };
 
 app.use(session(sess));
-app.use(passport.authenticate('session'));
-const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers });
-
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/images/');
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      new Date().getFullYear().toString() +
-        (new Date().getMonth() + 1).toString() +
-        new Date().getDate().toString() +
-        new Date().getHours().toString() +
-        new Date().getMinutes().toString() +
-        new Date().getSeconds().toString() +
-        file.originalname.slice(-4)
-    );
-  },
-});
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/jpeg' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/gif'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter,
-});
-
-app.post('/upload/send_file', upload.array('img', 1), (req, res, next) => {
-  let file_name =
-    new Date().getFullYear().toString() +
-    (new Date().getMonth() + 1).toString() +
-    new Date().getDate().toString() +
-    new Date().getHours().toString() +
-    new Date().getMinutes().toString() +
-    new Date().getSeconds().toString();
-  req.files[0].originalname.slice(-4);
-  //res.status(201).send();
-  res.redirect('/');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(require('./controllers/'));
+// turn on routes
+app.use(routes);
 
 // Google Outh
 passport.use(
